a583527fd58e3c0bfb3660253487a5162c8f842e
Praveen Sharma <piyush20112@gmail.com>
2026-07-21 17:45:16 +0530

    Adding data cleaning script and cleaned data csv

--- data-cleaning.py
+++ data-cleaning.py
@@ -16,10 +16,15 @@ df['rate_per_sqft'] = df['rate_per_sqft'].str.replace(',','').astype(float)
 cols_to_clean = ['status', 'property_type', 'builder_name','locality', 'socity', 'company_name', 'flat_type']
 for x in cols_to_clean:
     df[x] = df[x].str.strip().str.lower()
+
+df['rera_approval'] = df['rera_approval'].str.strip().map({'Not approved by RERA' : False,'Approved by RERA':True})
+df['status'] = df['status'].replace({'new':'ready to move','resale':'ready to move'})
+
 # outliars
 df = df.dropna(subset=['bhk_count','price'])
 df = df.query('bhk_count<=7')
 
 # print(len(df))
-print(df.head(10))
+# print(df.head(10))
 
+df.to_csv('cleaned_dataset.csv',index=0)
